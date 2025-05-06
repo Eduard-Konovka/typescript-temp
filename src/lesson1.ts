d231469da700f70c763e1c2ed866a80a2f679c8d
Eduard Konovka <ed098ua@gmail.com>
2025-05-06 23:18:12 +0300

    Complites Lesson 1

--- src/lesson1.ts
+++ src/lesson1.ts
@@ -1,21 +1,29 @@
-let total: number = 5;
-let userName: string = 'John';
-let isActive: boolean = true;
-let numbers: number[] = [1, 2, 3, 4, 5, 6]; 
-
 type User = {
   name: string;
   age: number;
   isActive: boolean;
-  hobbies: string[];
+  hobbies?: string[];
   address: {
     street: string;
     city: string;
     country: string;
   };
-  greet: () => void;
+  greet: (userName: string) => void;
 };
-    
+
+type size = 'small' | 'medium' | 'large';
+
+enum Sizes {
+  SMALL = 'small',
+  MEDIUM = 'medium',
+  LARGE = 'large',
+}
+
+let total: number = 5;
+let userName: string = 'John';
+let isActive: boolean = true;
+let numbers: number[] = [1, 2, 3, 4, 5, 6]; 
+
 const user: User = {
   name: 'John Doe',
   age: 30,
@@ -26,15 +34,50 @@ const user: User = {
     city: 'New York',
     country: 'USA',
   },
-  greet: function () {
-    console.log(`Hello, my name is ${this.name}`);
+  greet: function (userName: string = 'User') {
+    const hobby = this.hobbies ? this.hobbies[0] : 'no hobbies';
+
+    console.log(`Hello, my name is ${this.name}, my nickname is ${userName}, and I am ${this.age} years old, my hobbi is ${hobby}.`);
   }
 };
 
-type size = 'small' | 'medium' | 'large';
+const admin: User = {
+  name: 'Jane Doe',
+  age: 25,
+  isActive: false,
+  address: {
+    street: '456 Elm St',
+    city: 'Los Angeles',
+    country: 'USA',
+  },
+  greet: function (userName: string = 'User') {
+    const hobby = this.hobbies ? this.hobbies[0] : 'no hobbies';
+
+    console.log(`Hello, my name is ${this.name}, my nickname is ${userName}, and I am ${this.age} years old, my hobbi is ${hobby}.`);
+  }
+};
+
+user.greet('Johnny');
+admin.greet('Janey');
+
 const shirtSize: size = 'medium'; // This is valid
 // const invalidSize: size = 'extra-large'; // This will cause a TypeScript error
+const shirtSizeEnum: Sizes = Sizes.MEDIUM; // This is valid
+// const invalidSizeEnum: Sizes = 'extra-large'; // This will cause a TypeScript error
 
-user.greet();
+function add(num1: number, num2: number): string {
+  return (num1 + num2).toString();    
+}
 
-export { total, userName, isActive, numbers, User, user, size, shirtSize };
+console.log(add(5, 10)); // Output: "15"
+
+function sum(num1: number, num2: number): void {
+  console.log (num1 + num2);    
+}
+
+sum(2, 3); // Output: 5
+
+function greetUser(user: User): void {
+  console.log(`Hello, ${user.name}!`);
+}
+greetUser(user); // Output: Hello, John Doe
