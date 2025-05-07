50379b56c76b29fe8e7f48241eb41e26bd9404ed
Eduard Konovka <ed098ua@gmail.com>
2025-05-07 23:49:14 +0300

    Continues module2

--- src/module2.ts
+++ src/module2.ts
@@ -7,3 +7,6 @@ let octal: number = 0o744;   // вісімкове
 function foo (num: number, str: string, bool: boolean, empty: null) {
   // Some logic
 }
+
+let mixed: (number | string)[] = [1, 'two'];
+let matrix: number[][] = [[1, 2], [3, 4]];
