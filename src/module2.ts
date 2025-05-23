a8d338bee3df4880e3f473f9e7898badff0c0bfc
Eduard Konovka <ed098ua@gmail.com>
2025-05-23 22:54:15 +0300

    Continues module2

--- src/module2.ts
+++ src/module2.ts
@@ -680,3 +680,24 @@ const user2: Person2 = {
 
 // Перевантаження операторів (function overloads) =================
 // Перевантаження операторів дозволяє вам визначити кілька версій функції з різними типами параметрів
+
+function combine3(input1: number, input2: number): number;
+function combine3(input1: string, input2: string): string;
+function combine3(input1: string, input2: number): string;
+function combine3(input1: number, input2: string): string;
+function combine3(input1: any, input2: any) {
+  if (typeof input1 === "number" && typeof input2 === "number") {
+    return input1 + input2;
+  } else if (typeof input1 === "string" && typeof input2 === "string") {
+    return input1 + input2;
+  } else if (typeof input1 === "string" && typeof input2 === "number") {
+    return input1 + input2;
+  } else if (typeof input1 === "number" && typeof input2 === "string") {
+    return input1 + input2;
+  }
+}
+
+console.log(combine3(1, 2)); // Outputs: 3
+console.log(combine3("Hello", "World")); // Outputs: HelloWorld
+console.log(combine3("Hello", 2)); // Outputs: Hello2
+console.log(combine3(1, "World")); // Outputs: 1World
