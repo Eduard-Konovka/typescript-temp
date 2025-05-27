c1160afb4383f552fcfa44d6b2977191ca5e955b
Eduard Konovka <ed098ua@gmail.com>
2025-05-27 22:49:46 +0300

    Continues module2

--- src/module2.ts
+++ src/module2.ts
@@ -716,3 +716,47 @@ let names = ["Alice", "Bob", "Charlie"];
 
 // console.log(concatenate(names)); // Outputs: AliceBobCharlie
 // console.log(concatenate(names, ", ")); // Outputs: Alice, Bob, Charlie
+
+type AdminType = {
+  type: "admin";
+  name: string;
+  privileges: string[];
+};
+
+type UserType = {
+  type: "user";
+  name: string;
+  registrationDate: Date;
+};
+
+function checkUser(name: string, type: "user"): UserType;
+function checkUser(
+  name: string,
+  type: "admin",
+  privileges: string[]
+): AdminType;
+function checkUser(
+  name: string,
+  type: "admin" | "user",
+  privilegesOrRegistrationDate?: string[]
+): AdminType | UserType {
+  if (type === "admin") {
+    return {
+      type,
+      name,
+      privileges: privilegesOrRegistrationDate as string[],
+    };
+  }
+
+  return {
+    type,
+    name,
+    registrationDate: new Date(),
+  };
+}
+
+const user3 = checkUser("Nikita", "user");
+const admin3 = checkUser("Tonya", "admin", ["manageUsers", "deletePosts"]);
+
+// console.log(user3.registrationDate);
+// console.log(admin3.privileges);
