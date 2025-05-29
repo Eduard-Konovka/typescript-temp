00697dd05783c7177104f13646f30be9885bc58b
Eduard Konovka <ed098ua@gmail.com>
2025-05-29 22:24:46 +0300

    Continues module2: Generics

--- src/generics.ts
+++ src/generics.ts
@@ -1,3 +1,15 @@
 let arr: Array<string | number> = [];
 
 arr = ["str", 10];
+
+const promise: Promise<string> = new Promise((resolve) => {
+  setInterval(() => {
+    resolve("Done!");
+  }, 1000);
+});
+
+promise.then((data): void => {
+  console.log(data);
+});
+
+// Generic function/method ==========================
