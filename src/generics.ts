98eb6a86d602da2423653efed3f80c993295ab32
Eduard Konovka <ed098ua@gmail.com>
2025-05-30 23:49:16 +0300

    Continues module2: Generics function/method

--- src/generics.ts
+++ src/generics.ts
@@ -13,3 +13,86 @@ promise.then((data): void => {
 });
 
 // Generic function/method ==========================
+
+function identity<T>(arg: T): T {
+  return arg;
+}
+
+let output1 = identity<string>("myString");
+let output2 = identity<number>(100);
+let output3 = identity<boolean>(true);
+let output4 = identity<string[]>(["a", "b", "c"]);
+let output5 = identity<{ name: string }>({ name: "John" });
+let output6 = identity<null>(null);
+let output7 = identity<undefined>(undefined);
+let output8 = identity<{}>({});
+let output9 = identity<never>(undefined as never); // Using 'never' type
+
+output1 = identity("myString");
+output2 = identity(100);
+
+console.log(
+  output1,
+  output2,
+  output3,
+  output4,
+  output5,
+  output6,
+  output7,
+  output8,
+  output9
+);
+
+function firstElement<T>(arr: T[]): T {
+  return arr[0];
+}
+
+let numbers2 = [1, 2, 3, 4, 5];
+let firstNum = firstElement(numbers2);
+
+let strings2 = ["a", "b", "c", "d"];
+let firstStr = firstElement(strings2);
+
+console.log(firstNum, firstStr);
+
+function merge(objA: object, objB: object) {
+  return Object.assign(objA, objB);
+}
+
+// const merged = merge({ name: "Alisa" }, { age: 28 });
+const merged = merge({ name: "Alisa" }, { age: 28 }) as {
+  name: string;
+  age: number;
+};
+
+merged.name;
+
+function merge2<T, U>(objA2: T, objB2: U) {
+  return Object.assign(objA2, objB2);
+}
+
+const merged2 = merge2({ name: "Alisa" }, { age: 28 });
+
+merged2.name;
+
+// const merged3: {
+//   name: string;
+// } & {
+//   age: number;
+// };
+
+type Person3 = {
+  name: string;
+};
+
+type AdditionFields = {
+  age: number;
+};
+
+function merge3<T, U>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+const merged3 = merge<Person3, AdditionFields>({ name: "Alisa" }, { age: 28 });
+
+merged3.name;
