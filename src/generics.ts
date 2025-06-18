4da3169c61f78cd7f3002b38ce09faf3fdff605d
Eduard Konovka <ed098ua@gmail.com>
2025-06-18 23:47:58 +0300

    Continues module2

--- src/generics.ts
+++ src/generics.ts
@@ -326,3 +326,52 @@ let assignment: Assignment = {
 console.log("Assignment:", assignment);
 
 // Record<K, T> ---------------------------------
+
+type Weekdays = "Mon" | "Tue" | "Wed" | "Thu" | "Fri";
+type Weekend = "Sat" | "Sun";
+
+type Day = Weekdays | Weekend;
+
+type DayTranslations = Record<Day, string>;
+
+const translations: DayTranslations = {
+  Mon: "Понеділок",
+  Tue: "Вівторок",
+  Wed: "Середа",
+  Thu: "Четверг",
+  Fri: "П'ятниця",
+  Sat: "Субота",
+  Sun: "Неділя",
+};
+
+console.log("Translations:", translations);
+
+//
+
+enum UserRoles {
+  admin = "admin",
+  manager = "manager",
+  employee = "employee",
+}
+
+type UserRolesStatuses = Record<UserRoles, boolean>;
+
+const userRoleStatuses: UserRolesStatuses = {
+  [UserRoles.admin]: true,
+  [UserRoles.manager]: false,
+  [UserRoles.employee]: true,
+};
+
+//
+
+type InitialFormType = {
+  name: string;
+  email: string;
+  password: string;
+};
+
+export type Form = InitialFormType & {
+  errors: Partial<Record<keyof InitialFormType, [string]>>;
+};
+
+// Omit<T, K> ---------------------------------
